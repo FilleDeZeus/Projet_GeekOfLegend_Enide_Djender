9972ba1d69db0d7039fbf97a3b79ffcdb9e95e36
FilleDeZeus <enide26@hotmail.fr>
2023-01-06 14:39:44 +0100

    methode Guerrier

--- public/js/module/guerrier.js
+++ public/js/module/guerrier.js
@@ -4,4 +4,16 @@ export class Guerrier extends Heros{
         super(nom, ptVIE, ptAttaque);
         this.ptRage = ptRage;
     }
-}
+    Rage() {
+        
+        if (this.ptRage < 4){
+            this.ptRage = this.ptRage + 1; // this.ptRage += ptRage
+        }
+        else if (this.ptRage ==4){
+            this.ptRage = 0;
+            return this.ptAttaque = this.ptAttaque * 1.25;
+        }else{
+            return this.ptAttaque;
+        }
+        }
+    }
