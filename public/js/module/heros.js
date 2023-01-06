2f475cdacaf84777efd7e6676cb337d293870750
FilleDeZeus <enide26@hotmail.fr>
2023-01-06 14:07:30 +0100

    methode hero

--- public/js/module/heros.js
+++ public/js/module/heros.js
@@ -3,5 +3,16 @@ export class Heros{
         this.nom =nom;
         this.ptVie = ptVie;
         this.ptAttaque = ptAttaque;
+        this.action = action;
     }
+    Attaque(){
+        this.ptAttaque = this.ptAttaque * 1.4;
+        this.ptVie = this.ptVie * 0.75;
+        this.action = "attaque";
+    }
+    Defense(){
+        this.ptAttaque = this.ptAttaque * 1.4;
+        this.ptVie = this.ptVie * 0.75;
+        this.action = "defense";
+    }      
 }
