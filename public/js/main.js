d66fb77918dd673f58c480c0a098780ac415e5a1
FilleDeZeus <enide26@hotmail.fr>
2023-01-09 08:38:36 +0100

    amélioration code

--- public/js/main.js
+++ public/js/main.js
@@ -74,24 +74,24 @@ function combat() {
         if (guerrier.ptVie > 0) {
             alert(`${guerrier.nom} attaque ${boss.nom}`);
             boss.ptVie -= guerrier.ptAttaque;
-            alert(`${boss.nom} perd ${guerrier.ptAttaque} points de vie. Il lui reste ${boss.ptVie}pv`);
+            alert(`${boss.nom} perd ${guerrier.ptAttaque} points de vie. Il lui reste ${boss.ptVie} point de vie `);
         }
 
         if (archer.ptVie > 0 && boss.ptVie > 0 && archer.fleches >= 2) {
             alert(`${archer.nom} attaque ${boss.nom}`)
             boss.ptVie -= archer.ptAttaque;
-            alert(`${boss.nom} perd ${archer.ptAttaque} points de vie. Il lui reste ${boss.ptVie}pv`);
+            alert(`${boss.nom} perd ${archer.ptAttaque} points de vie. Il lui reste ${boss.ptVie}  points de vie `);
             archer.nbreFleche();
         } else if (archer.ptVie > 0 && archer.fleches < 2) {
-            alert(`${archer.nom} n'a plus de flèches. Il passe un tour.`)
+            alert(`${archer.nom} n'a plus de flèches. Il passe son tour.`)
         }
 
         if (mage.ptVie > 0 && boss.ptVie > 0 && mage.mana >= 2) {
             alert(`${mage.nom} attaque ${boss.nom}`)
             boss.ptVie -= mage.ptAttaque;
-            alert(`${boss.nom} perd ${mage.ptAttaque} points de vie. Il lui reste ${boss.ptVie}pv`);
+            alert(`${boss.nom} perd ${mage.ptAttaque} points de vie. Il lui reste ${boss.ptVie}  points de vie `);
         } else if (mage.ptVie > 0 && mage.mana < 2) {
-            alert(`${mage.nom} n'a plus de mana. Il passe un tour.`)
+            alert(`${mage.nom} n'a plus de mana. Il passe  son tour.`)
         }
 
         //Choix de la victime du boss
@@ -105,9 +105,9 @@ function combat() {
             alert(`${boss.nom} attaque ${defenseur.nom}`);
             defenseur.ptVie -= boss.ptAttaque;
             if (defenseur.ptVie >= 0) {
-                alert(`${defenseur.nom} perd ${boss.ptAttaque} points de vie. Il lui reste ${defenseur.ptVie}pv`);
+                alert(`${defenseur.nom} perd ${boss.ptAttaque} points de vie. Il lui reste ${defenseur.ptVie}  points de vie `);
             } else {
-                alert(`Attaque critique! ${defenseur.nom} n'a plus de pv.`);
+                alert(`Attaque critique! ${defenseur.nom} n'a plus de points de vie .`);
             }
         }
 
@@ -116,12 +116,12 @@ function combat() {
         }
     }
     if (boss.ptVie < vingt) {
-        alert(`${boss.nom} est bientôt mort. En dernière resource, il lance énigme. Si tu trouves la réponse tu gagnes, sinon, tu perds.`);
+        alert(`${boss.nom} est bientôt mort. Pour l'achever répond à cette énigme. Si tu trouves la réponse tu gagnes, sinon, tu perds.`);
         boss.enigme(guerrier, mage, archer);
     }
 
     if (boss.ptVie <= 0) {
-        alert(`Tu as vaincu ${boss.nom}. Félicitations!`);
+        alert(`Tu as gagné ${boss.nom}. Félicitations!`);
     } else if (guerrier.ptVie <= 0 && archer.ptVie <= 0 && mage.ptVie <= 0) {
         alert(`Tu as été vaincu. Un peu plus d'entrainement et la prochaine sera peut être la bonne`);
     }
